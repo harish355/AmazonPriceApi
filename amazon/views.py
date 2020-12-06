368bb190600706f9d20c4792e99df016c726c6cf
Harish_kumar_hk <40381981+harish355@users.noreply.github.com>
2020-12-06 19:00:58 +0530

    Update views.py
    Changed Header for Request

--- amazon/views.py
+++ amazon/views.py
@@ -4,8 +4,18 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+def get_headers():
+    return {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+            "cache-control": "max-age=0",
+            "dnt": "1",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"}
 
 
 @api_view(["POST", "GET"])
@@ -22,7 +32,7 @@ def index(request):
         except:
             pass
 
-        page = requests.get(url, headers=headers)
+        page = requests.get(url, headers=get_headers())
         soup = BeautifulSoup(page.content, 'html.parser')
         
         try:
